185c6d2d17d8c219def296b9291dcf682441cbc8
Alli Wong <wonga10@gmail.com>
2021-12-07 19:04:07 -0800

    Setup Day8

---
+++ Day8/Day8.cs
@@ -0,0 +1,16 @@
+﻿namespace Aoc2021;
+
+class Day8
+{
+    public static readonly List<int> Input = File.ReadAllText("Day8/input").Split(",").Select(int.Parse).ToList();
+
+    public static long Part1()
+    {
+        throw new NotImplementedException();
+    }
+
+    public static long Part2()
+    {
+        throw new NotImplementedException();
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,5 +1,5 @@
 ﻿
 using Aoc2021;
 
-Console.WriteLine(Day7.Part2());
+Console.WriteLine(Day8.Part1());
 Console.Read();
