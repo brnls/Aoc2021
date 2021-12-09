a72d12cf9e508de569389fa5bf342a663c8a4051
Alli Wong <wonga10@gmail.com>
2021-12-08 16:22:33 -0800

    Day8part1

--- Day8/Day8.cs
+++ Day8/Day8.cs
@@ -2,11 +2,13 @@
 
 class Day8
 {
-    public static readonly List<int> Input = File.ReadAllText("Day8/input").Split(",").Select(int.Parse).ToList();
+    public static readonly List<string> Input = File.ReadAllLines("Day8/input").ToList();
 
     public static long Part1()
     {
-        throw new NotImplementedException();
+        var acceptedSegments = new List<int> { 2, 3, 4, 7 };
+        var outputValues = Input.Select(x => x.Split('|')[1]).ToList();
+        return outputValues.Select(row => row.Split().Where(word => acceptedSegments.Contains(word.Length)).Count()).Sum();
     }
 
     public static long Part2()
