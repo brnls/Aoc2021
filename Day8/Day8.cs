7d0d6f2b03af5d971630f8dca30d9a8151dc231e
brnls <brnls1234@gmail.com>
2021-12-08 21:01:38 -0800

    part 2 wip

--- Day8/Day8.cs
+++ Day8/Day8.cs
@@ -13,6 +13,36 @@ class Day8
 
     public static long Part2()
     {
-        throw new NotImplementedException();
+        Dostuff(Input[0]);
+        return 0;
     }
+
+    public static void DoStuff(string line)
+    {
+        var legend = new char[7];
+        var lineSplit = line.Split("|");
+        var legendNums = lineSplit[0].Split();
+
+        var one = legendNums.Single(x => x.Length == 2);
+        var four = legendNums.Single(x => x.Length == 4);
+        var seven = legendNums.Single(x => x.Length == 3);
+        var eight = legendNums.Single(x => x.Length == 7);
+        var sixOrZeroOrNine = legendNums.Where(x => x.Length == 6);
+        var six = sixOrZeroOrNine.Single(x => x.Except(one).Count() == 5);
+        var zeroOrNine = sixOrZeroOrNine.Where(x => x != six);
+
+
+        legend[0] = seven.Except(one).Single();
+        legend[1] = eight.Except(six).Single();
+        legend[2] = one.Where(x => x != legend[1]).Single();
+
+        var nine = zeroOrNine.Single(x => x.Except(four.Concat(new char[] { legend[0] })).Count() == 1);
+        var zero = zeroOrNine.Single(x => x != nine);
+
+        legend[3] = nine.Except(four.Concat(new char[] { legend[0] })).SingleOrDefault();
+        legend[4] = eight.Except(nine).Single();
+        legend[6] = eight.Except(zero).Single();
+        legend[5] = eight.Except(legend).Single();
+    }
+
 }
--- Program.cs
+++ Program.cs
@@ -1,5 +1,5 @@
 ﻿
 using Aoc2021;
 
-Console.WriteLine(Day8.Part1());
+Console.WriteLine(Day8.Part2());
 Console.Read();
